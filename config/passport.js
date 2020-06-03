5fb079581d113038840d720ca81ef24ff8c2f532
Ramsay <ramsaygonzalez@outlook.com>
2020-06-02 21:24:05 -0400

    added user table model for sequelize.

--- config/passport.js
+++ config/passport.js
@@ -6,21 +6,18 @@ const db = require("../models");
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(
   new LocalStrategy(
-    // Our user will sign in using an email, rather than a "username"
-    {
-      usernameField: "email"
-    },
-    (email, password, done) => {
+
+    (userName, password, done) => {
       // When a user tries to sign in this code runs
       db.User.findOne({
         where: {
-          email: email
+          userName: userName
         }
       }).then(dbUser => {
-        // If there's no user with the given email
+        // If there's no user with the given username
         if (!dbUser) {
           return done(null, false, {
-            message: "Incorrect email."
+            message: "Incorrect username."
           });
         }
         // If there is a user with the given email, but the password the user gives us is incorrect
--- models/user.js
+++ models/user.js
@@ -1,25 +1,59 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require("bcryptjs");
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define("User", {
-    // The email cannot be null, and must be a proper email before creation
-    email: {
+
+    userID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    adminID: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    adminUser: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    activeUser: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    userName: {
       type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
+      allowNull: false
     },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
       allowNull: false
-    }
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    middleInitial: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
